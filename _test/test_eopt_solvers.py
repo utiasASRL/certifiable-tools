3205f4ff8a052b5800d8d1148cea1d5ecd58b6b8
Connor Holmes <connor.holmes@mail.utoronto.ca>
2023-09-15 17:35:21 -0400

    get_subgradient function written and passing tests.

---
+++ _test/test_eopt_solvers.py
@@ -0,0 +1,59 @@
+from cert_tools.eopt_solvers import *
+# Maths
+import numpy as np
+import scipy.sparse as sp
+
+def test_subgradient():
+    # Define eigenvalues and vectors
+    eig_vals = [-1., 1., 1., 3.]
+    D = np.diag(eig_vals)
+    T = np.random.rand(4,4)*2-1
+    # Make orthogonal matrix from T
+    Q, R = np.linalg.qr(T)
+    # Define Test Matrix
+    H = Q @ D @ Q.T
+    # Constraint matrices
+    A = []
+    A += [sp.diags([1.,0.,0.,0.])]
+    A += [sp.diags([0.,1.,0.,0.])]
+    # Compute subgrad and actual subgrad (with default U)
+    subgrad, min_eig = get_subgradient(H,A,k=4,method="direct")
+    subgrad_true =np.array([ Q[0,0]**2, Q[1,0]**2 ])
+    # Check length
+    assert len(subgrad) == len(A), \
+        ValueError("Subgradient should have length equal to that of constraints")
+    # Check eig
+    np.testing.assert_almost_equal(min_eig, -1.)
+    # Check subgradient
+    np.testing.assert_allclose(subgrad,subgrad_true, rtol=0 , atol=1e-8)
+
+def test_subgradient_mult2():
+    "Multiplicity 2 test"
+    # Define eigenvalues and vectors 
+    eig_vals = [-1., -1., 1., 3.]
+    D = np.diag(eig_vals)
+    T = np.random.rand(4,4)*2-1
+    # Make orthogonal matrix from T
+    Q, R = np.linalg.qr(T)
+    # Define Test Matrix
+    H = Q @ D @ Q.T
+    # Constraint matrices
+    A = []
+    A += [sp.diags([1.,0.,0.,0.])]
+    A += [sp.diags([0.,1.,0.,0.])]
+    # Compute subgrad and actual subgrad (with default U)
+    subgrad, min_eig = get_subgradient(H,A,k=4,method="direct")
+    subgrad_true =np.array([ Q[0,0]**2 + Q[0,1]**2, Q[1,0]**2 + Q[1,1]**2 ])/2.
+    # Check length
+    assert len(subgrad) == len(A), \
+        ValueError("Subgradient should have length equal to that of constraints")
+    # Check eig
+    np.testing.assert_almost_equal(min_eig, np.min(eig_vals))
+    # Check subgradient
+    np.testing.assert_allclose(subgrad,subgrad_true, rtol=0 , atol=1e-8)
+    
+    
+    
+if __name__ == "__main__":
+    test_subgradient()
+    test_subgradient_mult2()
--- cert_tools/eopt_solvers.py
+++ cert_tools/eopt_solvers.py
@@ -4,16 +4,23 @@ import cvxpy as cp
 
 # Maths
 import numpy as np
+import scipy.sparse as sp
 from cert_tools.eig_tools import get_min_eigpairs
 
 
-def get_subgradient(Q, A_list, a):
-    H_curr = Q + np.sum([ai * Ai for ai, Ai in zip(a, A_list)])
-
-    eig_vals, eig_vecs = get_min_eigpairs(H_curr)
-    U = 1 / Q.shape[0] * np.eye(Q.shape[0])
-    return eig_vecs @ U @ eig_vecs.T
-
+def get_subgradient(H, A_list, U=None, tau=1e-8, **kwargs):
+    eig_vals, eig_vecs = get_min_eigpairs(H, **kwargs)
+    # get minimum eigenvalue     
+    min_eig = np.min(eig_vals)
+    # get minimum eigenvectors (multiplicity could be > 1)
+    inds = np.abs(eig_vals - min_eig) < tau
+    Q = eig_vecs[:,inds]
+    # Scale variable
+    if U is None:
+        U = 1 / Q.shape[1] * np.eye(Q.shape[1])
+    # Compute gradient
+    subgrad = [np.trace(U @ (Q.T @ Ai @ Q)) for Ai in A_list]
+    return subgrad, min_eig
 
 def solve_Eopt_QP(Q, A_list, a_init=None, max_iters=500, gtol=1e-7):
     """Solve max_alpha sigma_min (Q + sum_i (alpha_i * A_i)), using the QP algorithm
@@ -41,3 +48,22 @@ def solve_Eopt_QP(Q, A_list, a_init=None, max_iters=500, gtol=1e-7):
             success = False
     info = {"success": success, "msg": msg}
     return a, info
+
+def solve_eopt_penalty(Q, Constraints, x_cand, opts, verbose=True):
+    """Solve the certificate/eigenvalue optimization problem using a penalty term
+    to enforce the affine constraints (i.e. first order opt conditions). """
+    
+    # Get affine constraint from constraints and candidate solution
+    A_bar = sp.hstack([A @ x_cand for A,b in Constraints])
+    b_bar = Q @ x_cand
+    # TODO Check sign here ^^^
+    # Initialize
+    min_eig = -np.inf
+    aff_cost = inf
+    n_iter = 0
+    x = np.zeros(A_bar.shape[1])
+    while (min_eig < -opts['tol_eig'] or aff_cost > opts['tol_aff']) and \
+        n_iter < opts['max_iter']:
+        # Construct Certificate matrix
+        H_curr = Q + np.sum([ai * Ai for ai, (Ai,b) in zip(x,Constraints)])
+        # Compute subgradient of ei  
