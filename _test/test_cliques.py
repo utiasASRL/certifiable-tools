0aceffcfec4fd072cca252b1aff27162d4e9a9d1
Frederike Duembgen <frederike.dumbgen@inria.fr>
2024-11-21 15:44:53 +0100

    Create new clique testing

--- _test/test_cliques.py
+++ _test/test_cliques.py
@@ -4,59 +4,70 @@ import os
 import numpy as np
 from poly_matrix import PolyMatrix
 
+from cert_tools import HomQCQP
 from cert_tools.base_clique import BaseClique
 
 root_dir = os.path.abspath(os.path.dirname(__file__) + "/../")
 
 
-def generate_random_X(seed=0):
+def generate_random_matrix(seed=0):
     np.random.seed(seed)
-    n_landmarks = 4
-    dim_x = 4
-    dim_z = 6
+    dim_x = 2
     X = PolyMatrix()
-    variables = {"h": 1, "x": dim_x}
-    variables.update({f"z_{j}": dim_z for j in range(n_landmarks)})
-    for pair in itertools.combinations_with_replacement(variables, 2):
-        print(pair)
-        fill = np.random.rand(variables[pair[0]], variables[pair[1]])
-        if pair[0] == pair[1]:
-            fill += fill.T
-        X[pair[0], pair[1]] = fill
+    for i in range(1, 4):
+        random_fill = np.random.rand(1 + 2 * dim_x, 1 + 2 * dim_x)
+        random_fill += random_fill.T
+        clique, __ = PolyMatrix.init_from_sparse(
+            random_fill, {"h": 1, f"x_{i}": dim_x, f"x_{i+1}": dim_x}
+        )
+        clique.symmetric = True
+        X += clique
     return X
 
 
-def test_overlap():
-    X = generate_random_X()
-    clique_vars = [
-        ["h", "x", "z_0"],
-        ["h", "x", "z_1"],
-        ["h", "x", "z_0", "z_1"],
-        ["h", "x", "z_1", "z_2"],
-        ["h", "x", "z_0", "z_2"],
+def test_decompositions():
+    def test_cost(cliques, C_gt):
+        C = PolyMatrix()
+        mat_decomp = problem.decompose_matrix(problem.C, method="split")
+        for clique in cliques:
+            C += mat_decomp[clique.index]
+        np.testing.assert_allclose(C.get_matrix_dense(variables), C_gt)
+
+    problem = HomQCQP()
+    problem.C = generate_random_matrix()
+    problem.As = []
+    variables = problem.C.get_variables()
+    C_gt = problem.C.get_matrix_dense(variables)
+
+    # will create a clique decomposition that is not always the same
+    problem.clique_decomposition()
+    test_cost(problem.cliques, C_gt)
+
+    clique_list = [
+        {"h", "x_1", "x_2"},
+        {"h", "x_2", "x_3"},
+        {"h", "x_3", "x_4"},
     ]
-    clique_list = []
-    for vars in clique_vars:
-        var_dict = {v: X.variable_dict_i[v] for v in vars}
-        X_k = X.get_matrix(variables=var_dict, output_type="dense")
-        clique_list.append(BaseClique(var_size=var_dict, X=X_k, Q=None))
+    problem.clique_decomposition(clique_data=clique_list)
+    test_cost(problem.cliques, C_gt)
+    for var_list, clique in zip(clique_list, problem.cliques):
+        assert set(clique.var_list).difference(var_list) == set()
 
-    # sanity check
-    c0 = clique_list[0]
-    assert isinstance(c0, BaseClique)
-    assert c0.get_ranges("h") == [[[0], [0]]]
-    assert c0.get_ranges("x") == [
-        [[0], list(range(1, X.variable_dict_i["x"] + 1))]
-    ], c0.get_ranges("x")
-
-    for cl, ck in itertools.combinations(clique_list, 2):
-        overlap = BaseClique.get_overlap(cl, ck)
-        for l in overlap:
-            print(f"testing overlap {l} of {cl}, {ck}")
-            for rl, rk in zip(cl.get_ranges(l), ck.get_ranges(l)):
-                np.testing.assert_allclose(cl.X[rl[0], rl[1]], ck.X[rk[0], rk[1]])
+    clique_data = {
+        "cliques": [{"h", "x_1", "x_2"}, {"h", "x_2", "x_3"}, {"h", "x_3", "x_4"}],
+        "separators": [{"h", "x_2"}, {"h", "x_2", "x_3"}, {"h", "x_3"}],
+        "parents": [
+            1,
+            2,
+            2,
+        ],  # parent of 0 is 1, parent of 1 is 2, parent of 2 is itself (it's the root)
+    }
+    problem.clique_decomposition(clique_data=clique_data)
+    test_cost(problem.cliques, C_gt)
+    for var_list, clique in zip(clique_list, problem.cliques):
+        assert set(clique.var_list).difference(var_list) == set()
 
 
 if __name__ == "__main__":
-    test_overlap()
+    test_decompositions()
     print("all tests passed.")
