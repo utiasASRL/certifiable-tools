f37d8293826be50b7e47d3d95a3122cf054ab974
newtype51 <connor.holmes@mail.utoronto.ca>
2025-03-25 16:40:30 -0400

    Added some initial tests for rank inflation.

--- _scripts/mw_slam_reduction.py
+++ _scripts/mw_slam_reduction.py
@@ -10,8 +10,16 @@ from sdprlayers.utils.camera_model import CameraModel
 from sdprlayers.utils.lie_algebra import so3_exp, so3_log, so3_wedge
 
 from cert_tools import HomQCQP
-from cert_tools.rank_reduction import get_low_rank_factor, rank_reduction
+from cert_tools.rank_reduction import (
+    get_low_rank_factor,
+    rank_inflation,
+    rank_inflation_lr,
+    rank_inflation_pg,
+    rank_inflation_sdp,
+    rank_reduction,
+)
 from cert_tools.sdp_solvers import (
+    adjust_Q,
     solve_lambda_cvxpy,
     solve_low_rank_sdp,
     solve_sdp_fusion,
@@ -75,6 +83,10 @@ def prune_constraints(C, Constraints, first_redun, xhat, mult_cutoff=10):
     return constraints_out
 
 
+def sym_dim(n):
+    return n * (n + 1) / 2
+
+
 def run_test(test=4, load_solns=True, plot=False):
     # Load problems
     df_1 = load_dataset(fname="_examples/mw_slam_10pose_r1.pkl")
@@ -84,12 +96,18 @@ def run_test(test=4, load_solns=True, plot=False):
     # Make the variable orderings consistent
     prob_h.get_asg(var_list=prob_l.var_list)
 
+    # Get constraints
+    C, constraints = get_cost_constraints(prob_h)
+    C_l, constraints_l = get_cost_constraints(prob_l)
+
     # Run optimizations
     if load_solns:
         with open("saved_soln.pkl", "rb") as handle:
             data = pickle.load(handle)
             X_l = data["X_l"]
             X_h = data["X_h"]
+            X_pr = data["X_pr"]
+            constraints_pr = data["constraints_pr"]
     else:
         X_l, info_l, solve_time_l = solve_sdp_homqcqp(
             problem=prob_l, verbose=True, adjust=True
@@ -97,20 +115,41 @@ def run_test(test=4, load_solns=True, plot=False):
         X_h, info_h, solve_time_h = solve_sdp_homqcqp(
             problem=prob_h, verbose=True, adjust=True
         )
-        data = dict(X_l=X_l, X_h=X_h, info_l=info_l, info_h=info_h)
+        # Get rank-1 solution
+        V_l, r = get_low_rank_factor(X_l, rank_tol=RANK_TOL)
+        # Prune constraints to just get cost tightness
+        first_redun = len(constraints) - 1
+        constraints_pr = prune_constraints(
+            C=C,
+            Constraints=constraints_l,
+            first_redun=first_redun,
+            xhat=V_l,
+            mult_cutoff=0.1,
+        )
+        # Resolve optimization problem with fewer constraints
+        X_pr, info_pr = solve_sdp_fusion(
+            Q=C, Constraints=constraints_pr, adjust=True, verbose=True
+        )
+        # Save data
+        data = dict(
+            X_l=X_l,
+            X_h=X_h,
+            X_pr=X_pr,
+            info_pr=info_pr,
+            constraints_pr=constraints_pr,
+            info_l=info_l,
+            info_h=info_h,
+        )
         with open("saved_soln.pkl", "wb") as handle:
             pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
     # Get low rank factors
-    V_l, r = get_low_rank_factor(X_l, rank_tol=RANK_TOL)
-    V_h, r = get_low_rank_factor(X_h, rank_tol=RANK_TOL)
-
-    # Get constraints
-    C, constraints = get_cost_constraints(prob_h)
-    C_l, constraints_l = get_cost_constraints(prob_l)
-    # Optimal cost
+    V_l = get_low_rank_factor(X_l, rank_tol=RANK_TOL)
+    V_h = get_low_rank_factor(X_h, rank_tol=RANK_TOL)  # Optimal cost
     p_opt = np.trace(C @ X_l)
+    # Adjusted C
+    C_adj, scale, offset = adjust_Q(C)
 
+    # TESTS
     if test == 1:
         # Test 1: Check if range spaces overlap and reduce rank
         result = np.linalg.lstsq(V_h, V_l)
@@ -120,41 +159,26 @@ def run_test(test=4, load_solns=True, plot=False):
         else:
             print("Low rank solution is in the range space of high rank solution")
 
-        # Test 2: Check if the rank of the high rank solution can be reduced
+        # Check if the rank of the high rank solution can be reduced
         V_h_red = rank_reduction(constraints, X_h, rank_tol=RANK_TOL, verbose=True)
     elif test == 2:
-        # Test 3: Check if the rank can be reduced if we use the redundant constraints as well
+        # Test 2: Check if the rank can be reduced if we use the redundant constraints as well
         V_h_red = rank_reduction(constraints_l, X_h, rank_tol=RANK_TOL, verbose=True)
     elif test == 3:
-        # Test 4: Try to prune constraints so that we just get cost tightness.
-        # Get new solution with pruned constraints. Reduce rank.
-
-        # Prune Constraints
-        first_redun = len(constraints) - 1
-        constraints_pruned = prune_constraints(
-            C=C,
-            Constraints=constraints_l,
-            first_redun=first_redun,
-            xhat=V_l,
-            mult_cutoff=0.1,
-        )
-        # Resolve optimization problem
-        X_pruned, info = solve_sdp_fusion(
-            Q=C, Constraints=constraints_pruned, adjust=True, verbose=True
-        )
+        # Test 3: Try to prune constraints so that we just get cost tightness and evaluate whether RRA works
         # Get optimality gap
-        d_opt = np.trace(C @ X_pruned)
+        d_opt = np.trace(C @ X_pr)
         # Check if low rank solution is in the range space of this solution
-        V_p, _ = get_low_rank_factor(X_pruned, rank_tol=RANK_TOL)
+        V_p, _ = get_low_rank_factor(X_pr, rank_tol=RANK_TOL)
         result = np.linalg.lstsq(V_p, V_l)
         residual = result[1]
         print(f"Range space check residual: {residual}")
-        print(f"Reduced to {len(constraints_pruned)}/{len(constraints_l)} constraints")
+        print(f"Reduced to {len(constraints_pr)}/{len(constraints_l)} constraints")
         print(f"Certified optimal cost:{p_opt}")
         print(f"Optimal cost with pruned constraints: {d_opt}")
         print("Applying rank reduction with pruned constraints...")
         V_pr = rank_reduction(
-            constraints_pruned, X_pruned, rank_tol=RANK_TOL, null_tol=1e-5, verbose=True
+            constraints_pr, X_pr, rank_tol=RANK_TOL, null_tol=1e-5, verbose=True
         )
         # Check gap to redundant solution and constraint violation
         viol, cost_diff = check_feasibility(V_pr @ V_pr.T, p_opt, C, constraints)
@@ -167,15 +191,15 @@ def run_test(test=4, load_solns=True, plot=False):
         print(f"Mag of V_l: {mag_l}, Mag of V_pr {mag_pr}")
         print(f"Angle between vectors: {angle}")
         # Check if same certificate works
-        H = info["H"]
+        H = data["info_pr"]["H"]
         print(f"Strong duality check at solution, V' @ H @ V = 0: {V_pr.T @ H@ V_pr}")
         print(f"Null space check, max(abs(H @ V)) = 0: {np.max(np.abs(H@ V_pr))}")
         if plot:
             # compare eigenvalues of certificates
-            vals_l = np.linalg.eigvalsh(data['info_l']['H'])
+            vals_l = np.linalg.eigvalsh(data["info_l"]["H"])
             vals_p = np.linalg.eigvalsh(H)
-            plt.semilogy(vals_l,'.-', label="Rank Tight Certificate")
-            plt.semilogy(vals_p,'.-', label="Cost Tight Certificate")
+            plt.semilogy(vals_l, ".-", label="Rank Tight Certificate")
+            plt.semilogy(vals_p, ".-", label="Cost Tight Certificate")
             plt.title("Dual/Certificate Eigenvalues")
             plt.legend()
             plt.grid(True)
@@ -183,16 +207,14 @@ def run_test(test=4, load_solns=True, plot=False):
         V_h = rank_reduction(
             constraints, X_h, rank_tol=RANK_TOL, null_tol=1e-5, verbose=False
         )
-        
-        
-        
+
         # Print out costs
         print(f"Costs")
         print("=========")
         print(f"No Redundant: {np.trace(C@X_h)}")
         print(f"No Redundant (reduced): {np.trace(V_h.T @ C @ V_h)}")
         print(f"All Redundant: {np.trace(C@X_l)}")
-        print(f"Cost Tight: {np.trace(C@X_pruned)}")
+        print(f"Cost Tight: {np.trace(C@X_pr)}")
         print(f"Cost Tight (reduced): {np.trace(V_pr.T @ C @ V_pr)}")
 
         # Plot eigenvalues for different cases
@@ -200,7 +222,7 @@ def run_test(test=4, load_solns=True, plot=False):
             eigvals_l = np.linalg.eigvalsh(X_l)
             eigvals_h = np.linalg.eigvalsh(X_h)
             eigvals_h_red = np.linalg.eigvalsh(V_h @ V_h.T)
-            eigvals_pruned = np.linalg.eigvalsh(X_pruned)
+            eigvals_pruned = np.linalg.eigvalsh(X_pr)
             eigvals_pruned_red = np.linalg.eigvalsh(V_pr @ V_pr.T)
 
             # Sort eigenvalues in descending order and take the largest 10
@@ -224,7 +246,7 @@ def run_test(test=4, load_solns=True, plot=False):
             plt.grid(True)
             plt.show()
     elif test == 4:
-        # Test Low rank approximation of the solution
+        # Compare feasibility of RRA then round, versus just round.
         # Get rank reduction solution
         V_rr = rank_reduction(
             constraints, X_h, rank_tol=RANK_TOL, targ_rank=1, verbose=True
@@ -238,10 +260,72 @@ def run_test(test=4, load_solns=True, plot=False):
         print(f"TR: {cost_diff_tr}, RR: {cost_diff_rr}")
         print("Maximum Constraint Violation")
         print(f"TR: {np.max(np.abs(viol_tr))}, RR: {np.max(np.abs(viol_rr))}")
+    elif test == 5:
+        # Check if we can reconstruct a certificate from complementary slackness of the cost tight solution
 
+        # Get optimality gap
+        d_opt = np.trace(C @ X_pr)
+        # Low rank factor of of the cost tight solution
+        V_p, r = get_low_rank_factor(X_pr, rank_tol=1e-6)
+        # normalize the vectors
+        # V_p = V_p / np.linalg.norm(V_p, axis=0, keepdims=True)
+        # Check if we have a theoretically unique solution
+        m = len(constraints_pr)
+        n = V_p.shape[0]
+        print(
+            f"If the solution is unique then ({sym_dim(n-r)} <= {sym_dim(n)-m}) is true"
+        )
+
+        # Construct Constraint matrix
+        A_bar = []
+        for A, b in constraints_pr:
+            A_bar.append(np.reshape(A @ V_p, (-1, 1), order="F"))
+        A_bar = np.hstack(A_bar)
+        c_bar = np.reshape(C_adj @ V_p, (-1, 1), order="F")
+        # Check rank of constraint gradient
+        u, s, v = np.linalg.svd(A_bar)
+        print(f"Rank of constraint gradient matrix: {np.sum(s > s[0]*RANK_TOL)}")
+        print(f"Number of constraints: {len(constraints_pr)}")
+        # Solve for multipliers
+        result = np.linalg.lstsq(A_bar, -c_bar)
+        mults = result[0]
+        residual = result[1]
+        # Check that the residual is low
+        print(f"Multiplier least squares solution residual: {np.abs(residual)}")
+        # Construct certificate
+        H = C_adj.toarray()
+        for i, (A, b) in enumerate(constraints_pr):
+            H += mults[i, 0] * A.toarray()
+        vals = np.linalg.eigvalsh(H)
+        # Print information
+        print(
+            f"Complementary Slackness (H@X) residual for rank-{r} solution: {np.trace(V_pr.T@ H @ V_pr)}"
+        )
+        print(
+            f"Complementary Slackness (H@X) residual for rank-1 solution: {np.trace(H @ X_l)}"
+        )
+        print(f"Minimum certificate eigenvalue: {np.min(vals)}")
+    elif test == 6:
+        # Test 6 : Rank inflation via SDP
+        X_max = rank_inflation_sdp(C, constraints_pr, X_l, verbose=True)
+        s = np.linalg.eigvalsh(X_max)
+        rank = np.sum(s > s[-1] * RANK_TOL)
+        print(f"Rank of max rank solution: {rank}")
+    elif test == 7:
+        # Test 7 : Rank inflation via projected grad
+        X_max = rank_inflation_pg(C, constraints_pr, X_l, verbose=True)
+        s = np.linalg.eigvalsh(X_max)
+        rank = np.sum(s > s[-1] * RANK_TOL)
+        print(f"Rank of max rank solution: {rank}")
+    elif test == 8:
+        # Test 8 : Rank inflation via projected grad
+        X_max = rank_inflation(C, constraints_pr, X_l, verbose=True)
+        s = np.linalg.eigvalsh(X_max)
+        rank = np.sum(s > s[-1] * RANK_TOL)
+        print(f"Rank of max rank solution: {rank}")
     else:
         raise ValueError("Test unknown")
 
 
 if __name__ == "__main__":
-    run_test(test=4, load_solns=True, plot=True)
+    run_test(test=8, load_solns=True, plot=True)
--- cert_tools/rank_reduction.py
+++ cert_tools/rank_reduction.py
@@ -1,7 +1,11 @@
 import casadi as cas
 import matplotlib.pyplot as plt
+import mosek.fusion as mf
 import numpy as np
+import scipy.sparse as sp
+import sparseqr as sqr
 from diffcp.cones import unvec_symm, vec_symm
+from scipy.linalg import cholesky, lstsq, solve
 from scipy.sparse.linalg import svds
 
 from cert_tools.linalg_tools import get_nullspace
@@ -25,7 +29,8 @@ def rank_reduction(
 
     """
     # Get initial low rank factor
-    V, r = get_low_rank_factor(X_hr, rank_tol)
+    V = get_low_rank_factor(X_hr, rank_tol)
+    r = V.shape[1]
     if verbose:
         print(f"Initial rank of solution: {r}")
     # Get constraint operator matrix
@@ -69,6 +74,267 @@ def rank_reduction(
     return V
 
 
+def rank_inflation(
+    C,
+    Constraints,
+    X_lr,
+    max_iter=100,
+    alpha=0.25,
+    beta=0.99,
+    lr_approx=True,
+    damp=1e-4,
+    verbose=False,
+    debug=False,
+):
+    """Inflate the rank of a SDP solution using an analytic center technique.
+    Applies equality constrained optimization with objective equal to -log-det"""
+    # Unzip constraint list
+    As, bs = zip(*Constraints)
+    A_list = list(As)
+    b_list = list(bs)
+    # Add a constraint on cost
+    A_list.append(C)
+    b_list.append(np.trace(C @ X_lr))
+    b = np.array(b_list)[:, None]
+    # Shapes
+    m = len(A_list)
+    n = A_list[0].shape[0]
+
+    # Init
+    X = X_lr + 1e-5 * np.eye(n)
+    # X = np.eye(n)
+    n_iter = 0
+    C = np.zeros((m, m))
+    d = np.zeros((m, 1))
+    while n_iter < max_iter:
+        # Compute AX products
+        AX = [A @ X for A in A_list]
+        if lr_approx:
+            V = get_low_rank_factor(X)
+            VAV = [V.T @ A @ V for A in A_list]
+        # Construct linear system for multipliers
+        for i in range(m):
+            d[i] = np.trace(AX[i])
+            for j in range(i, m):
+                if lr_approx:
+                    C[i, j] = -np.trace(VAV[i] @ VAV[j])
+                else:
+                    C[i, j] = -np.sum(AX[i].T * AX[j])
+                if j > i:
+                    C[j, i] = C[i, j]
+        # Solve symmetric linear system
+        # w = solve(C, b - 2 * d, assume_a="sym")
+        res = lstsq(C + damp * np.eye(m), b - 2 * d)
+        w = res[0]
+        # Compute search direction
+        del_X = X - X @ np.sum([A_list[i] * w[i, 0] for i in range(m)]) @ X
+        decrement = np.linalg.norm(del_X)
+        if debug:
+            # full step should yield primal residual of zero.
+            X_plus = X + del_X
+            viol = [np.trace(A_list[i] @ X_plus) - b[i, 0] for i in range(m)]
+            print(f"DEBUG: max violation: {np.max(np.abs(viol))}")
+        # Backtracking linesearch
+        L = cholesky(X)
+        L_inv = solve(L, np.eye(n))
+        # These eigenvalues determine our search length
+        evals = np.linalg.eigvalsh(L_inv @ del_X @ L_inv)
+        eval_min = np.min(evals)
+        if eval_min < 0:
+            t_init = beta * min(-1 / eval_min, 1)
+        else:
+            t_init = 1
+        t = backtrack_linesearch(evals, alpha, beta, t_init)
+        # Update
+        X = X + t * del_X
+        n_iter = n_iter + 1
+        # print results
+        if verbose:
+            res_pri = np.linalg.norm(b - d)
+            print(f"Iter {n_iter}:  Newt.decr: {decrement},   res_pri: {res_pri}")
+
+    return X
+
+
+def backtrack_linesearch(evals, alpha, beta, t_init):
+    """Backtracking linesearch for analytic centering problem"""
+    # logdetX = np.log(np.linalg.det(X))
+    grad_f = -np.sum(evals)
+    t = t_init
+
+    bt_cond = True
+    # func_0 = -logdetX
+    func_0 = 0
+    while bt_cond:
+        # Compute objective at t
+        # func_t = -logdetX - np.sum(np.log(1 + t * evals))
+        func_t = -np.sum(np.log(1 + t * evals))
+        assert func_t is not np.nan, ValueError(
+            "Negative eigenvalue in backtrack computation"
+        )
+        # Check condition
+        bt_cond = func_t > func_0 + alpha * t * grad_f
+        # Update if necessary
+        if bt_cond:
+            t = beta * t
+
+    return t
+
+
+def rank_inflation_sdp(C, Constraints, X_lr, verbose=False):
+    """Inflate the rank keeping cost and constraint values the same using a feasibility problem."""
+    # Add cost constraint to list
+    Constraints_cost = Constraints.copy()
+    Constraints_cost.append((C, np.trace(C @ X_lr)))
+    # Starting point
+    eps = 1e-6
+    X_init = X_lr + eps * np.eye(X_lr.shape[0])
+
+    # Solve Feasibility problem
+    options = dict()
+    X_maximal, info = solve_sdp_fusion(
+        Q=C * 0,
+        Constraints=Constraints_cost,
+        adjust=False,
+        verbose=verbose,
+        options=options,
+        X_init=X_init.flatten(),
+    )
+
+    return X_maximal
+
+
+def get_sparse_nullspace(A, tolerance=1e-9):
+    # We "solve" a least squares problem to get the rank and permutations. This is the cheapest way to use sparse QR, since it does not require explicit construction of the Q matrix. We can't do this with qr function because the "just return R" option is not exposed.
+    Z, R, E, rank = sqr.rz(A, np.zeros((A.shape[0], 1)), tolerance=tolerance)
+    if not rank == A.shape[0]:
+        raise ValueError("QR decomposition rank is incorrect")
+    # Get nullspace
+    R = sp.csc_array(R)
+    R11, R12 = R[:rank, :rank], R[:rank, rank:]
+    # [R11  R12]  @  [R11^-1 @ R12] = [R12 - R12] = 0
+    # [0    0 ]       [    -I    ]    [0]
+    N = sp.vstack(
+        [sp.linalg.spsolve_triangular(R11, R12.todense()), -sp.eye(R12.shape[1])]
+    )
+    # Unpermute the basis
+    basis = N[E, :]
+
+    return basis
+
+
+def rank_inflation_lmi(
+    C,
+    Constraints,
+    X_lr,
+    eps=1e-3,
+    null_tol=1e-8,
+    max_iter=100,
+    verbose=False,
+):
+    """Inflate the rank using a projected gradient approach"""
+    # # Get constraint operator
+    A_bar = get_full_constraint_op(Constraints, C)
+    # # Compute nullspace using sparse rank revealing QR
+    F = get_sparse_nullspace(A_bar, tolerance=null_tol)
+
+    # Initial decomposition
+    V_0 = get_low_rank_factor(X_lr)
+    n_iter = 0
+    n, rank = V_0.shape
+    V = V_0
+
+
+def rank_inflation_pg(
+    C,
+    Constraints,
+    X_lr,
+    eps=1e-2,
+    max_iter=100,
+    alpha=0.25,
+    beta=0.8,
+    verbose=False,
+):
+    """Inflate the rank using a projected gradient approach"""
+    # Get constraint operator
+    A_bar = get_full_constraint_op(Constraints, C)
+
+    # Initial decomposition
+    V_0 = get_low_rank_factor(X_lr)
+    # Iterate
+    n_iter = 0
+    n, rank = V_0.shape
+    V = V_0
+    while n_iter < max_iter:
+        # GRADIENT COMPUTATION
+        # Compute gradient of log det function using SMW trick
+        D = eps * np.eye(rank) + V.T @ V
+        DinvV = np.linalg.solve(D, V.T)
+        Grad = 1 / eps * (np.eye(n) - V @ DinvV)
+        # NULL SPACE PROJECTION
+        grad = Grad.reshape((-1, 1))
+        grad_proj = nullspace_projection(A_bar, grad)
+        del_X = grad_proj.reshape((n, n))
+        # LINESEARCH
+        L = cholesky(V @ V.T + eps * np.eye(n))
+        L_inv = solve(L, np.eye(n))
+        # These eigenvalues determine our search length
+        evals = np.linalg.eigvalsh(L_inv @ del_X @ L_inv)
+        eval_min = np.min(evals)
+        if eval_min < 0:
+            t_init = beta * min(-1 / eval_min, 1)
+        else:
+            t_init = 1
+        t = backtrack_linesearch(evals, alpha, beta, t_init)
+        # Update and refactor
+        X = V @ V.T + t * del_X
+        V = get_low_rank_factor(X)
+        r = V.shape[1]
+        n_iter += 1
+        if verbose:
+            print(f"Iter {n_iter}, rank: {r}, alpha: {alpha}")
+
+
+def get_full_constraint_op(Constraints, C):
+    """Function to compute the constraint operator whose nullspace characterizes the optimal solution.
+    Computes the full operator rather than a subspace."""
+    A_bar = []
+    for A, b in Constraints:
+        A_bar.append(A.tocoo().reshape((1, -1)))
+    A_bar.append(C.tocoo().reshape((1, -1)))
+    A_bar = sp.vstack(A_bar)
+    return A_bar
+
+
+def nullspace_projection(A, x, method="direct"):
+    """Solve the nullspace projection for a large sparse matrix. That is we want to find:
+    x_p = (I - A^T (A A^T)^(-1) A) x = x - (A^+) A x
+
+    We solve this by defining y = A x, then solving A z = y as a least squares problem
+    """
+
+    if method == "cg":
+        Ax = A @ x
+        # construct normal eq matrix
+        AAt = A @ A.T
+        z, info = sp.linalg.cg(AAt, Ax)
+        # Projection
+        x_proj = x - A.T @ z[:, None]
+    elif method == "lsqr":
+        Ax = A @ x
+        output = sp.linalg.lsqr(A, Ax)
+        z = output[0]
+        x_proj = x - z[:, None]
+    elif method == "direct":
+        Ax = A @ x
+        # construct normal eq matrix
+        AAt = A @ A.T
+        z = np.linalg.solve(AAt.toarray(), Ax)
+        # Projection
+        x_proj = x - A.T @ z
+    return x_proj
+
+
 def rank_reduction_logdet(
     Constraints,
     X_hr,
@@ -84,7 +350,8 @@ def rank_reduction_logdet(
     """
 
     # Get low rank factor
-    V, r = get_low_rank_factor(X_hr, rank_tol)
+    V = get_low_rank_factor(X_hr, rank_tol)
+    r = V.shape[1]
     # Get reduced constraints
     reduced_constraints = get_reduced_constraints(Constraints, V)
 
@@ -113,11 +380,12 @@ def rank_reduction_logdet(
         n_iter += 1
 
         if verbose:
-            _, rank_s = get_low_rank_factor(S)
+            S_lr = get_low_rank_factor(S)
+            rank_s = S_lr.shape[1]
             print(f"ITER {n_iter}, DELTA {delta_S_norm}, RANK {rank_s}")
 
     # Compute low rank factor of reduced system
-    V_s, _ = get_low_rank_factor(S)
+    V_s = get_low_rank_factor(S)
     # Compute final low rank factor
     Y = V @ V_s
 
@@ -152,7 +420,7 @@ def get_low_rank_factor(X, rank_tol=1e-6, rank=None):
         rank = np.sum(vals > rank_tol * val_max)
     n = X.shape[0]
     V = vecs[:, (n - rank) :] * np.sqrt(vals[(n - rank) :])
-    return V, rank
+    return V
 
 
 def get_reduced_constraints(Constraints, V):
